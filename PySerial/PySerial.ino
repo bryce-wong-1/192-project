dff92935c3c3d79aada769c94d30e413e48405e2
Bryce Wong <brycewong1@gmail.com>
2024-01-28 22:43:14 -0800

    Python input - C++ MC configures input and sends it to circuit - Serial Read

---
+++ PySerial/PySerial.ino
@@ -0,0 +1,27 @@
+#include <LiquidCrystal.h>
+int x;
+LiquidCrystal lcd(11,10,5,4,3,2);
+int counter = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.setTimeout(1);
+  pinMode(8, INPUT);
+  lcd.begin(16,2);
+  lcd.clear();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (!Serial.available())
+  {
+    if(digitalRead(8) == HIGH || Serial.readString() == "y"){
+      counter++;
+      lcd.setCursor(3,1);
+      lcd.print(counter);
+      delay(200);
+    }
+  }
+  x = Serial.readString().toInt();
+  Serial.print(x+1);
+}
